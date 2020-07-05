11c89813f1052c93c9833b4341fe83ea34bd2682
Horváth Barnabás <barnabas.horvath@enet.hu>
2020-07-05 12:38:42 +0200

    get all AccessLog details

--- src/api/subscribeAccessLogs.ts
+++ src/api/subscribeAccessLogs.ts
@@ -4,8 +4,28 @@ export const subscribeAccessLogsGQL = gql`
   subscription accessLogs($input: AccessLogsInput) {
     accessLogs(input: $input) {
       direction
+      source {
+        name
+        namespace
+        workload
+        serviceAccount
+      }
+      destination {
+        name
+        namespace
+        workload
+        serviceAccount
+      }
       protocolVersion
       latency
+      request {
+        method
+        scheme
+        path
+      }
+      response {
+        statusCode
+      }
     }
   }
 `;
