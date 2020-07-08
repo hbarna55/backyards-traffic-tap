e1168bb8d84dbdd46e07df0ac8290255a7d0bd98
Horváth Barnabás <barnabas.horvath@enet.hu>
2020-07-08 10:07:09 +0200

    change font globaly

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider } from "@apollo/react-hooks";
 import Index from "pages/Index";
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 import ApolloClients from "utils/ApolloClients";
 
 const client = ApolloClients.getHttpsClient();
@@ -9,8 +10,21 @@ const App = () => {
   return (
     <ApolloProvider client={client}>
       <Index />
+      <GlobalStyle />
     </ApolloProvider>
   );
 };
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: Roboto,Helvetica Neue,sans-serif;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100vh;
+}
+`;
 
 export default App;
