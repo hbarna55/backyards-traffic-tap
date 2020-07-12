13fa85b18cebacb4341175ba6e3c28a80ec9e103
Horváth Barnabás <barnabas.horvath@enet.hu>
2020-07-12 16:09:15 +0200

    remove unnecessary colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,18 +2,11 @@ const colors = {
   blue: "#2a439c",
   blueLight: "#2a439c44",
   green: "#388e3c",
-  greenDark: "#3f9275",
   red: "#FE1616",
-  cyan: "#22CEB5",
-  lime: "#DCF107",
-  yellow: "#ffeb3b",
-  orange: "#FF9800",
   grayLight: "#F6F8F9",
   gray: "#DFE5E4",
   grayDark: "#4D625E",
-  grayDarkest: "#274A45",
   white: "#FFF",
-  black: "#000",
 };
 
 export default colors;
